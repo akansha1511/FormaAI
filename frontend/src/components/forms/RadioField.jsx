80c9df6667f743e0d10a603d5ad7d939577a4557
PrajjwalKumar02 <prajjwalkumar.it@gmail.com>
2026-07-31 23:51:20 +0530

    Refactor RadioField component layout and styles
    Refactor RadioField component to improve styling and structure.

--- frontend/src/components/forms/RadioField.jsx
+++ frontend/src/components/forms/RadioField.jsx
@@ -3,7 +3,6 @@ import { useFormContext } from "react-hook-form";
 import FieldWrapper from "./FieldWrapper";
 
 const RadioField = ({ field }) => {
-
     const {
         register,
         formState: { errors },
@@ -16,26 +15,22 @@ const RadioField = ({ field }) => {
             required={field.required}
             error={errors[field.id]}
         >
-
-            {field.options?.map((option) => (
-
-                <label
-                    key={option.value || option}
-                    className="radio-option"
-                >
-
-                    <input
-                        type="radio"
-                        value={option.value || option}
-                        {...register(field.id, field.validation || {})}
-                    />
-
-                    {option.label || option}
-
-                </label>
-
-            ))}
-
+            <div className="space-y-2">
+                {field.options?.map((option) => (
+                    <label
+                        key={option.value || option}
+                        className="flex items-center gap-2 text-sm text-gray-700 cursor-pointer"
+                    >
+                        <input
+                            type="radio"
+                            value={option.value || option}
+                            className="w-4 h-4 text-blue-600 focus:ring-blue-500"
+                            {...register(field.id, field.validation || {})}
+                        />
+                        {option.label || option}
+                    </label>
+                ))}
+            </div>
         </FieldWrapper>
     );
 };
