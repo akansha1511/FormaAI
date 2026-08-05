fc8cd868e253795189d684ca5715bbaad5d8873e
PrajjwalKumar02 <prajjwalkumar.it@gmail.com>
2026-08-05 14:38:31 +0530

    Refactor service exports for better structure

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,23 +1,26 @@
-export * from './api';
-export * from './authService';
-export * from './incidentService';
-export * from './formService';
-export * from './aiService';
-export * from './uploadService';
+// Export all services
+export { default as api } from './api';
+export { authService } from './authService';
+export { incidentService } from './incidentService';
+export { formService } from './formService';
+export { aiService } from './aiService';
+export { uploadService } from './uploadService';
 
 // Default export for convenience
-import * as api from './api';
-import * as authService from './authService';
-import * as incidentService from './incidentService';
-import * as formService from './formService';
-import * as aiService from './aiService';
-import * as uploadService from './uploadService';
+import api from './api';
+import { authService } from './authService';
+import { incidentService } from './incidentService';
+import { formService } from './formService';
+import { aiService } from './aiService';
+import { uploadService } from './uploadService';
 
-export default {
+const services = {
     api,
-    authService,
-    incidentService,
-    formService,
-    aiService,
-    uploadService
+    auth: authService,
+    incidents: incidentService,
+    forms: formService,
+    ai: aiService,
+    upload: uploadService
 };
+
+export default services;
